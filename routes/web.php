6a779b53fbe45dd73070f8020c4ac5485c3c760a
AliAmin565 <techali565@gmail.com>
2023-06-03 06:00:23 +0300

    Removing  Model TopNav

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\TopNav;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [TopNav::class,'index'])->name('home');
